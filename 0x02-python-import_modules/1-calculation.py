58e12ca7cc29d36038c595b092c9756e173bdceb
xAbu042 <abumapesa.ak@gmail.com>
2022-06-02 15:06:45 +0300

    Checking Qn1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "___main__":
+    '''
+        Doing Maths after importing functions from a file
+        and printing the result
+    '''
+
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
