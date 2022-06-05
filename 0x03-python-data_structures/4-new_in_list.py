49e658fe16bb0df07292510666982ba44baa7cd2
xAbu042 <abumapesa.ak@gmail.com>
2022-06-05 14:00:58 +0300

    Fixing errors

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def new_in_list(my_list,idx,element):
+def new_in_list(my_list, idx, element):
     '''
     Replaces an element in a list at a specific position
     without modifying the original list(like in C)
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@
 
 def multiple_returns(sentence):
     """
-    A function that returns a tuple with 
+    A function that returns a tuple with
     the length of a string and its first character.
     """
     if sentence == "":
