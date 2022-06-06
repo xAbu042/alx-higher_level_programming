ddee3bd5d59b4c8c57a88bbf65df007bd87b85f5
xAbu042 <abumapesa.ak@gmail.com>
2022-06-06 17:21:59 +0300

    Fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def print_list_integer(my_list=[]):
     '''
     Prints all the integers of a list
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-
+    '''
+    Prints a matrix of integers
+    '''
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end="")
